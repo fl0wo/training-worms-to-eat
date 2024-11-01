1bf1a0605c7bfe9c8f2b54d0cbbcdeb8d483b997
flo <sabaniflorian@gmail.com>
2024-11-01 15:26:59 +0100

    Added comment on starvation

--- src/worm/mod.rs
+++ src/worm/mod.rs
@@ -24,6 +24,7 @@ pub fn starve_worms(worms: &mut Vec<Worm>) {
     let factor = 0.001;
 
     for worm in worms.iter_mut() {
+        // TODO: this should not be a linear punishment, it should be exponential (sprinting consumes much more energy)
         worm.life -= factor * worm.speed;
     }
 
